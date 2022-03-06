d79e1152139159855f70e8b5c8f46db6e9958aba
YaRiK <98014616+WarriorOFlonelinesS@users.noreply.github.com>
2022-03-06 19:40:29 +0200

    Delete deposits.py

--- deposits.py
+++
@@ -1,8 +0,0 @@
-def bank(a,years):
-    # часть от суммы 
-    quart = (a * 10)/ 100
-    # проценты за каждый год  
-    percents = years * quart
-    sum = a + percents
-    return sum
-print(bank(5000,1))        
