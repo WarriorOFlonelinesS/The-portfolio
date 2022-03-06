ee044dc512e140dd983b7ce8d225a688b2232030
YaRiK <ydneprovskiy@gmail.com>
2022-03-06 17:19:15 +0200

    Add  deposit.py

---
+++ deposits.py
@@ -0,0 +1,8 @@
+def bank(a,years):
+    # часть от суммы 
+    quart = (a * 10)/ 100
+    # проценты за каждый год 
+    percents = years * quart
+    sum = a + percents
+    return sum
+print(bank(5000,1))
